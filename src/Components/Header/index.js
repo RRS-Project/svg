d9c76f3f4fe5f44421e876340b7f52978a0ad26b
Riya Savant <rieee2110@gmail.com>
2021-04-25 12:22:06 +0530

    Updated Header contents

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,8 +4,8 @@ export default function Header()
 {
     return(
         <div className="jumbotron">
-            <h1 className="display-4">DBN Compiler</h1>
-            <p className="lead">This is a simple compiler that compiles DBN language to generate corrosponding SVGs</p>
+            <h1 className="display-4">SVG Compiler</h1>
+            <p className="lead">This is a simple compiler that compiles DBN like code to SVG</p>
         </div>
     );
 }
